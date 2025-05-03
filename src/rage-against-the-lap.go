b36a8a7be0aba086d143ff19e9f7996c7cf06dc5
Nick Stewart <nickstewart@192.168.1.226>
2025-05-03 21:36:09 +0100

    parse html and print driver and race data

--- src/rage-against-the-lap.go
+++ src/rage-against-the-lap.go
@@ -8,11 +8,12 @@ import (
 	"net/mail"
 	"os"
 	"strings"
+	"unicode"
+
+	"golang.org/x/net/html"
 )
 
 func main() {
-	fmt.Println("Karting Rules!")
-
 	file, err := os.Open("race-result.eml")
 
 	if err != nil {
@@ -27,9 +28,8 @@ func main() {
 
 	buf := new(strings.Builder)
 
-	n, err := io.Copy(buf, m.Body)
+	_, err = io.Copy(buf, m.Body)
 
-	fmt.Printf("Copied: %d bytes \n\n", n)
 	if err != nil {
 		log.Panic(err.Error())
 	}
@@ -39,7 +39,7 @@ func main() {
 	scanner := bufio.NewScanner(strings.NewReader(body))
 
 	var isHtml bool = false
-	var html []string
+	var htmlString []string
 	for scanner.Scan() {
 		var line string = scanner.Text()
 
@@ -48,11 +48,120 @@ func main() {
 		}
 
 		if isHtml {
-			html = append(html, line)
+			htmlString = append(htmlString, line)
 			if strings.TrimRight(line, "\n") == "</html>" {
 				isHtml = false
 			}
 		}
 	}
-	fmt.Print(html)
+
+	rootNode, _ := html.Parse(strings.NewReader(strings.Join(htmlString, "")))
+
+	var tables []*html.Node = searchHtml(rootNode, "table", []*html.Node{})
+
+	var driverInfoHtml []*html.Node = searchHtml(tables[0], "tr", []*html.Node{})
+
+	var driverInfo DriverInfo = DriverInfo{
+		Name:     extractTextIter(driverInfoHtml[3])[1],
+		Pos:      stripPosition(extractTextIter(driverInfoHtml[4])[2]),
+		RaceType: extractTextIter(driverInfoHtml[6])[1],
+	}
+
+	fmt.Println(driverInfo)
+
+	var raceData []RaceData
+
+	for _, row := range searchHtml(tables[2], "tr", []*html.Node{}) {
+		if extractTextIter(row)[0] == driverInfo.Pos {
+			data := RaceData{
+				Pos:    extractTextIter(row)[0],
+				Kart:   extractTextIter(row)[1],
+				Racer:  driverInfo.Name,
+				Best:   extractTextIter(row)[2],
+				NoLaps: extractTextIter(row)[3],
+				Avg:    extractTextIter(row)[4],
+				Gap:    extractTextIter(row)[5],
+			}
+			raceData = append(raceData, data)
+		} else {
+			data := RaceData{
+				Pos:    extractTextIter(row)[0],
+				Kart:   extractTextIter(row)[1],
+				Racer:  extractTextIter(row)[2],
+				Best:   extractTextIter(row)[3],
+				NoLaps: extractTextIter(row)[4],
+				Avg:    extractTextIter(row)[5],
+				Gap:    extractTextIter(row)[6],
+			}
+			raceData = append(raceData, data)
+		}
+
+	}
+	for _, data := range raceData {
+		fmt.Println(data)
+	}
+}
+
+func searchHtml(n *html.Node, term string, result []*html.Node) []*html.Node {
+	if n.Type == html.ElementNode && n.Data == term {
+		result = append(result, n)
+	}
+
+	// Recursively visit children
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		result = searchHtml(c, term, result)
+	}
+
+	return result
+}
+
+func extractTextIter(n *html.Node) []string {
+	var data []string
+	// start with just the root
+	stack := []*html.Node{n}
+
+	for len(stack) > 0 {
+		// pop
+		node := stack[len(stack)-1]
+		stack = stack[:len(stack)-1]
+
+		if node.Type == html.TextNode {
+			txt := strings.TrimSpace(node.Data)
+			if txt != "" {
+				data = append(data, txt)
+			}
+		}
+
+		// push children in reverse so we process them in order
+		for c := node.LastChild; c != nil; c = c.PrevSibling {
+			stack = append(stack, c)
+		}
+	}
+	return data
+}
+
+func stripPosition(position string) string {
+	for index, char := range position {
+		fmt.Println(unicode.IsDigit(char))
+		if !unicode.IsDigit(char) {
+			return position[:index]
+		}
+	}
+	return position
+}
+
+type RaceData struct {
+	Pos    string
+	Kart   string
+	Racer  string
+	Best   string
+	NoLaps string
+	Avg    string
+	Gap    string
+}
+
+type DriverInfo struct {
+	Name     string
+	Pos      string
+	RaceType string
 }
